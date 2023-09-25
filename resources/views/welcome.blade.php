22cb8e4bc12b579250538550118f3b13c58da0ce
Takamanu <fauzanalivijsma@gmail.com>
2023-09-25 17:18:49 +0700

    feat: ui admin home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,9 +40,9 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Pemasukan ({{ $tahun }})</div>
+                                Total Siswa yang mendaftar</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                <input type="checkbox" checked data-toggle="toggle" data-onstyle="info">
+                                <h2>NULL</h2>
                             </div>
                         </div>
                         <div class="col-auto">
@@ -54,7 +54,7 @@
         </div>
 
         <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        {{-- <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-info shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -79,10 +79,10 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
 
         <!-- Pending Requests Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
+        {{-- <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -104,7 +104,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
     <!-- Content Row -->
 
