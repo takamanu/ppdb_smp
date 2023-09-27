12494ddb11323fefc20d85c0cce99c27634e1d60
Rafael Vito <rafaelvito11@outlook.co.id>
2023-09-27 13:57:56 +0700

    config untuk alur pendaftaran

---
+++ app/Models/Config.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Config extends Model
+{
+    use HasFactory;
+    protected $table = 'config';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'pendaftaran_akun_ppdb_start',
+        'pendaftaran_akun_ppdb_due',
+        'pengumpulan_berkas_start',
+        'pengumpulan_berkas_due',
+        'test_akademik_start',
+        'test_akademik_due',
+        'test_baca_al_quran_start',
+        'test_baca_al_quran_due',
+        'test_wawancara_start',
+        'test_wawancara_due',
+        'pendaftaran_ulang_start',
+        'pendaftaran_ulang_due',
+    ];
+}
