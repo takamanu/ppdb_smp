58a18b344e091ec7fdede6290dd0328a9863415c
Takamanu <fauzanalivijsma@gmail.com>
2023-09-30 11:35:37 +0700

    fix: bug midtrans 4

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -99,27 +99,29 @@ class PaymentController extends Controller
         $nilai = abs($nilai);
         $huruf = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas'];
         $temp = '';
+
         if ($nilai < 12) {
             $temp = ' ' . $huruf[$nilai];
         } elseif ($nilai < 20) {
-            $temp = penyebut($nilai - 10) . ' belas';
+            $temp = $this->penyebut($nilai - 10) . ' belas';
         } elseif ($nilai < 100) {
-            $temp = penyebut($nilai / 10) . ' puluh' . penyebut($nilai % 10);
+            $temp = $this->penyebut($nilai / 10) . ' puluh' . $this->penyebut($nilai % 10);
         } elseif ($nilai < 200) {
-            $temp = ' seratus' . penyebut($nilai - 100);
+            $temp = ' seratus' . $this->penyebut($nilai - 100);
         } elseif ($nilai < 1000) {
-            $temp = penyebut($nilai / 100) . ' ratus' . penyebut($nilai % 100);
+            $temp = $this->penyebut($nilai / 100) . ' ratus' . $this->penyebut($nilai % 100);
         } elseif ($nilai < 2000) {
-            $temp = ' seribu' . penyebut($nilai - 1000);
+            $temp = ' seribu' . $this->penyebut($nilai - 1000);
         } elseif ($nilai < 1000000) {
-            $temp = penyebut($nilai / 1000) . ' ribu' . penyebut($nilai % 1000);
+            $temp = $this->penyebut($nilai / 1000) . ' ribu' . $this->penyebut($nilai % 1000);
         } elseif ($nilai < 1000000000) {
-            $temp = penyebut($nilai / 1000000) . ' juta' . penyebut($nilai % 1000000);
+            $temp = $this->penyebut($nilai / 1000000) . ' juta' . $this->penyebut($nilai % 1000000);
         } elseif ($nilai < 1000000000000) {
-            $temp = penyebut($nilai / 1000000000) . ' milyar' . penyebut(fmod($nilai, 1000000000));
+            $temp = $this->penyebut($nilai / 1000000000) . ' milyar' . $this->penyebut(fmod($nilai, 1000000000));
         } elseif ($nilai < 1000000000000000) {
-            $temp = penyebut($nilai / 1000000000000) . ' trilyun' . penyebut(fmod($nilai, 1000000000000));
+            $temp = $this->penyebut($nilai / 1000000000000) . ' trilyun' . $this->penyebut(fmod($nilai, 1000000000000));
         }
+
         return $temp;
     }
 
