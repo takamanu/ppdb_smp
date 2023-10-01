1f54f4a561af1024bd3f0c82fd7491c670eef474
Takamanu <fauzanalivijsma@gmail.com>
2023-10-01 20:51:01 +0700

    feat: details on the admin index 2

--- resources/views/agen/index.blade.php
+++ resources/views/agen/index.blade.php
@@ -55,7 +55,7 @@
                                                 Belum bayar
                                             @elseif (is_null($item->payment))
                                                 Belum bayar
-                                            @elseif ($item->$payment->status_payment !== 2 && $item->$payment->status !== 2)
+                                            @elseif ($item->payment->status_payment !== 2 && $item->payment->status !== 2)
                                                 Proses bayar
                                             @else
                                                 Selesai bayar
