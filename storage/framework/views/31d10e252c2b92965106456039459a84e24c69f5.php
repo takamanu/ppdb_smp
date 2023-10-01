edd240aa32ded095691ab16b014994320d69ae8d
Takamanu <fauzanalivijsma@gmail.com>
2023-10-01 19:00:08 +0700

    fix: clear cache

--- storage/framework/views/31d10e252c2b92965106456039459a84e24c69f5.php
+++
@@ -1,9 +0,0 @@
-<!-- Footer -->
-<footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-            <span>Copyright &copy; PPDB SMP SMPTQ Pangeran Diponegoro </span>
-        </div>
-    </div>
-</footer>
-<!-- End of Footer --><?php /**PATH C:\Users\LENOVO\OneDrive\Desktop\ppdb_smp\resources\views/partials/footer.blade.php ENDPATH**/ ?>
