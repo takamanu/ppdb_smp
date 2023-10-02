682273e8f30e702cf895d5016cd44a104585c945
Takamanu <fauzanalivijsma@gmail.com>
2023-10-02 14:25:24 +0700

    fix: bug terakhir bismillah

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -102,9 +102,9 @@
         });
     
         // prevent form submit
-        const form = document.querySelector("form");
-        form.addEventListener('submit', function (e) {
-            e.preventDefault();
-        });
+        // const form = document.querySelector("form");
+        // form.addEventListener('submit', function (e) {
+        //     e.preventDefault();
+        // });
     </script>
 @endsection
