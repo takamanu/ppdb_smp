b4895db01140f5bad8e279e0e2c8e5fb3ebde7ac
Takamanu <fauzanalivijsma@gmail.com>
2023-10-02 23:25:29 +0700

    fix: use dropdown for nilai input

--- resources/views/agen/nilai.blade.php
+++ resources/views/agen/nilai.blade.php
@@ -41,7 +41,6 @@
                                     <option value="E" {{ $agen->nilai->matematika == 'E' ? 'selected' : '' }}>E
                                     </option>
                                 </select>
-
                                 @error('matematika')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -57,19 +56,12 @@
                                 <select id="ilmu_pengetahuan_alam"
                                     class="form-select form-control @error('ilmu_pengetahuan_alam') is-invalid @enderror"
                                     name="ilmu_pengetahuan_alam" required>
-                                    <option value="E" disabled
-                                        {{ old('ilmu_pengetahuan_alam') == 'E' ? 'selected' : '' }}>Pilih nilai A-E
-                                    </option>
-                                    <option value="A" {{ old('ilmu_pengetahuan_alam') == 'A' ? 'selected' : '' }}>A
-                                    </option>
-                                    <option value="B" {{ old('ilmu_pengetahuan_alam') == 'B' ? 'selected' : '' }}>B
-                                    </option>
-                                    <option value="C" {{ old('ilmu_pengetahuan_alam') == 'C' ? 'selected' : '' }}>C
-                                    </option>
-                                    <option value="D" {{ old('ilmu_pengetahuan_alam') == 'D' ? 'selected' : '' }}>D
-                                    </option>
-                                    <option value="E" {{ old('ilmu_pengetahuan_alam') == 'E' ? 'selected' : '' }}>E
-                                    </option>
+                                    <option value="E" disabled {{ $agen->nilai->ilmu_pengetahuan_alam == 'E' ? 'selected' : '' }}>Pilih nilai A-E</option>
+                                    <option value="A" {{ $agen->nilai->ilmu_pengetahuan_alam == 'A' ? 'selected' : '' }}>A</option>
+                                    <option value="B" {{ $agen->nilai->ilmu_pengetahuan_alam == 'B' ? 'selected' : '' }}>B</option>
+                                    <option value="C" {{ $agen->nilai->ilmu_pengetahuan_alam == 'C' ? 'selected' : '' }}>C</option>
+                                    <option value="D" {{ $agen->nilai->ilmu_pengetahuan_alam == 'D' ? 'selected' : '' }}>D</option>
+                                    <option value="E" {{ $agen->nilai->ilmu_pengetahuan_alam == 'E' ? 'selected' : '' }}>E</option>
                                 </select>
                                 @error('ilmu_pengetahuan_alam')
                                     <span class="invalid-feedback" role="alert">
@@ -86,18 +78,12 @@
                                 <select id="bahasa_indonesia"
                                     class="form-select form-control @error('bahasa_indonesia') is-invalid @enderror"
                                     name="bahasa_indonesia" required>
-                                    <option value="E" disabled {{ old('bahasa_indonesia') == 'E' ? 'selected' : '' }}>
-                                        Pilih nilai A-E</option>
-                                    <option value="A" {{ old('bahasa_indonesia') == 'A' ? 'selected' : '' }}>A
-                                    </option>
-                                    <option value="B" {{ old('bahasa_indonesia') == 'B' ? 'selected' : '' }}>B
-                                    </option>
-                                    <option value="C" {{ old('bahasa_indonesia') == 'C' ? 'selected' : '' }}>C
-                                    </option>
-                                    <option value="D" {{ old('bahasa_indonesia') == 'D' ? 'selected' : '' }}>D
-                                    </option>
-                                    <option value="E" {{ old('bahasa_indonesia') == 'E' ? 'selected' : '' }}>E
-                                    </option>
+                                    <option value="E" disabled {{ $agen->nilai->bahasa_indonesia == 'E' ? 'selected' : '' }}>Pilih nilai A-E</option>
+                                    <option value="A" {{ $agen->nilai->bahasa_indonesia == 'A' ? 'selected' : '' }}>A</option>
+                                    <option value="B" {{ $agen->nilai->bahasa_indonesia == 'B' ? 'selected' : '' }}>B</option>
+                                    <option value="C" {{ $agen->nilai->bahasa_indonesia == 'C' ? 'selected' : '' }}>C</option>
+                                    <option value="D" {{ $agen->nilai->bahasa_indonesia == 'D' ? 'selected' : '' }}>D</option>
+                                    <option value="E" {{ $agen->nilai->bahasa_indonesia == 'E' ? 'selected' : '' }}>E</option>
                                 </select>
                                 @error('bahasa_indonesia')
                                     <span class="invalid-feedback" role="alert">
@@ -114,19 +100,12 @@
                                 <select id="test_membaca_al_quran"
                                     class="form-select form-control @error('test_membaca_al_quran') is-invalid @enderror"
                                     name="test_membaca_al_quran" required>
-                                    <option value="E" disabled
-                                        {{ old('test_membaca_al_quran') == 'E' ? 'selected' : '' }}>Pilih nilai A-E
-                                    </option>
-                                    <option value="A" {{ old('test_membaca_al_quran') == 'A' ? 'selected' : '' }}>A
-                                    </option>
-                                    <option value="B" {{ old('test_membaca_al_quran') == 'B' ? 'selected' : '' }}>B
-                                    </option>
-                                    <option value="C" {{ old('test_membaca_al_quran') == 'C' ? 'selected' : '' }}>C
-                                    </option>
-                                    <option value="D" {{ old('test_membaca_al_quran') == 'D' ? 'selected' : '' }}>D
-                                    </option>
-                                    <option value="E" {{ old('test_membaca_al_quran') == 'E' ? 'selected' : '' }}>E
-                                    </option>
+                                    <option value="E" disabled {{ $agen->nilai->test_membaca_al_quran == 'E' ? 'selected' : '' }}>Pilih nilai A-E</option>
+                                    <option value="A" {{ $agen->nilai->test_membaca_al_quran == 'A' ? 'selected' : '' }}>A</option>
+                                    <option value="B" {{ $agen->nilai->test_membaca_al_quran == 'B' ? 'selected' : '' }}>B</option>
+                                    <option value="C" {{ $agen->nilai->test_membaca_al_quran == 'C' ? 'selected' : '' }}>C</option>
+                                    <option value="D" {{ $agen->nilai->test_membaca_al_quran == 'D' ? 'selected' : '' }}>D</option>
+                                    <option value="E" {{ $agen->nilai->test_membaca_al_quran == 'E' ? 'selected' : '' }}>E</option>
                                 </select>
                                 @error('test_membaca_al_quran')
                                     <span class="invalid-feedback" role="alert">
