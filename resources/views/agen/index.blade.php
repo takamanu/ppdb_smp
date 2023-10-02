97319bec74099bf50ac8b6e36e679d264d5cac14
Takamanu <fauzanalivijsma@gmail.com>
2023-10-02 23:32:31 +0700

    fix: dashboard admin

--- resources/views/agen/index.blade.php
+++ resources/views/agen/index.blade.php
@@ -1,145 +1,154 @@
 @extends('layouts.main')
 
 @section('container')
-    <div class="card p-5">
-        <div class="card-header">
-            <h2>Daftar Siswa</h2>
-        </div>
-        <div class="card-body">
-            <a href="{{ url('/agen/create') }}" class="btn btn-success btn-sm float-left" title="Add New Agen">
-                <i class="fa fa-plus" aria-hidden="true"></i> Tambah Siswa
-            </a>
-            <form method="GET"
-                class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search float-right">
-                <div class="input-group">
-                    <input type="text" class="form-control bg-light border-0 small" name="cari" id="cari"
-                        placeholder="Cari siswa..." value={{ $cari }}>
-                    <div class="input-group-append">
-                        <button class="btn btn-primary" type="submit" name="submit">
-                            <i class="fas fa-search fa-sm"></i>
-                        </button>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>Daftar Siswa</h2>
                     </div>
-                </div>
-            </form>
-            <br><br>
-            @if (session('status'))
-                <div class="alert alert-success">
-                    {{ session('status') }}
-                </div>
-            @endif
-            <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>@sortablelink('name', 'Nama')</th>
-                            <th>@sortablelink('email', 'Email')</th>
-                            <th>Status Bayar</th>
-                            <th>Status Datapokok</th>
-                            <th>Tanggal Dibuat</th>
-                            {{-- <th>Tanggal Diupdate</th> --}}
-                            {{-- <th>Role</th> --}}
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($agen as $key => $item)
-                            <tr>
-                                <td>{{ $agen->firstItem() + $key }}</td>
-                                <td>{{ $item->name }}</td>
-                                <td>{{ $item->email }}</td>
-                                <td>
-                                    @if ($item->payment == '[]')
-                                        Belum bayar
-                                        {{-- @elseif ($item->payment->status_payment !== 2 && $item->payment->status !== 2)
+                    <div class="card-body">
+                        <a href="{{ url('/agen/create') }}" class="btn btn-success btn-sm float-left" title="Add New Agen">
+                            <i class="fa fa-plus" aria-hidden="true"></i> Tambah Siswa
+                        </a>
+                        <form method="GET"
+                            class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search float-right">
+                            <div class="input-group">
+                                <input type="text" class="form-control bg-light border-0 small" name="cari"
+                                    id="cari" placeholder="Cari siswa..." value={{ $cari }}>
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="submit" name="submit">
+                                        <i class="fas fa-search fa-sm"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                        <br><br>
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <div class="table-responsive">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>@sortablelink('name', 'Nama')</th>
+                                        <th>@sortablelink('email', 'Email')</th>
+                                        <th>Status Bayar</th>
+                                        <th>Status Datapokok</th>
+                                        <th>Tanggal Dibuat</th>
+                                        {{-- <th>Tanggal Diupdate</th> --}}
+                                        {{-- <th>Role</th> --}}
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($agen as $key => $item)
+                                        <tr>
+                                            <td>{{ $agen->firstItem() + $key }}</td>
+                                            <td>{{ $item->name }}</td>
+                                            <td>{{ $item->email }}</td>
+                                            <td>
+                                                @if ($item->payment == '[]')
+                                                    Belum bayar
+                                                    {{-- @elseif ($item->payment->status_payment !== 2 && $item->payment->status !== 2)
                                                 Proses bayar --}}
-                                    @else
-                                        Selesai bayar
-                                    @endif
-                                    {{-- {{ $item->payment }} --}}
-                                </td>
-                                <td>
-                                    @if (empty($item->datapokok))
-                                        Belum isi datapokok
-                                    @elseif (is_null($item->datapokok))
-                                        Belum isi datapokok
-                                    @else
-                                        Isi datapokok
-                                    @endif
-                                    {{-- {{ $item->datapokok }} --}}
-                                </td>
-                                <td>{{ $item->created_at }}</td>
-                                {{-- <td>{{ $item->updated_at }}</td> --}}
-                                {{-- <td>{{ $item->role }}</td> --}}
-                                {{-- @if ($item->role == '1') 
+                                                @else
+                                                    Selesai bayar
+                                                @endif
+                                                {{-- {{ $item->payment }} --}}
+                                            </td>
+                                            <td>
+                                                @if (empty($item->datapokok))
+                                                    Belum isi datapokok
+                                                @elseif (is_null($item->datapokok))
+                                                    Belum isi datapokok
+                                                @else
+                                                    Isi datapokok
+                                                @endif
+                                                {{-- {{ $item->datapokok }} --}}
+                                            </td>
+                                            <td>{{ $item->created_at }}</td>
+                                            {{-- <td>{{ $item->updated_at }}</td> --}}
+                                            {{-- <td>{{ $item->role }}</td> --}}
+                                            {{-- @if ($item->role == '1') 
                                             <td>Admin</td>
                                         @else
                                             <td>Member</td>
                                         
                                         @endif --}}
-                                <td>
-                                    @if (empty($item->datapokok))
-                                        <form method="POST" action="{{ url('/agen' . '/' . $item->id) }}"
-                                            accept-charset="UTF-8" style="display:inline">
-                                            {{ method_field('DELETE') }}
-                                            {{ csrf_field() }}
-                                            <button type="submit" class="btn btn-danger btn-sm" title="Delete Siswa"
-                                                onclick="return confirm(&quot;Apakah anda ingin menghapus data siswa {{ $item->name }}?&quot;)"><i
-                                                    class="fa fa-trash" aria-hidden="true"></i></button>
-                                        </form>
-                                    @elseif (is_null($item->datapokok))
-                                        <form method="POST" action="{{ url('/agen' . '/' . $item->id) }}"
-                                            accept-charset="UTF-8" style="display:inline">
-                                            {{ method_field('DELETE') }}
-                                            {{ csrf_field() }}
-                                            <button type="submit" class="btn btn-danger btn-sm" title="Delete Siswa"
-                                                onclick="return confirm(&quot;Apakah anda ingin menghapus data siswa {{ $item->name }}?&quot;)"><i
-                                                    class="fa fa-trash" aria-hidden="true"></i></button>
-                                        </form>
-                                    @else
-                                        <a href="{{ url('/agen/nilai/' . $item->id) }}"
-                                            title="Tambah Kelulusan Siswa"><button class="btn btn-success btn-sm"><i
-                                                    class="fa fa-plus" aria-hidden="true"></i></button></a>
-                                        <a href="{{ url('/agen/cetak/' . $item->id) }}" title="Cetak Datapokok Siswa">
-                                            <button class="btn btn-warning btn-sm"><i class="fa fa-print"
-                                                    aria-hidden="true"></i></button>
-                                        </a>
-                                        <a href="{{ url('/agen/' . $item->id) }}" title="Lihat Siswa"><button
-                                                class="btn btn-info btn-sm"><i class="fa fa-eye"
-                                                    aria-hidden="true"></i></button></a>
-                                        {{-- <a href="{{ url('/agen/' . $item->id . '/edit') }}" title="Edit Agen"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a> --}}
-                                        <form method="POST" action="{{ url('/agen' . '/' . $item->id) }}"
-                                            accept-charset="UTF-8" style="display:inline">
-                                            {{ method_field('DELETE') }}
-                                            {{ csrf_field() }}
-                                            <button type="submit" class="btn btn-danger btn-sm" title="Delete Siswa"
-                                                onclick="return confirm(&quot;Apakah anda ingin menghapus data siswa {{ $item->name }}?&quot;)"><i
-                                                    class="fa fa-trash" aria-hidden="true"></i></button>
-                                        </form>
-                                    @endif
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                <div class="row mb-5">
-                    <div class="col-md-8">
-                        Showing {{ $agen->firstItem() }} to {{ $agen->lastItem() }} of {{ $agen->total() }}
+                                            <td>
+                                                @if (empty($item->datapokok))
+                                                    <form method="POST" action="{{ url('/agen' . '/' . $item->id) }}"
+                                                        accept-charset="UTF-8" style="display:inline">
+                                                        {{ method_field('DELETE') }}
+                                                        {{ csrf_field() }}
+                                                        <button type="submit" class="btn btn-danger btn-sm"
+                                                            title="Delete Siswa"
+                                                            onclick="return confirm(&quot;Apakah anda ingin menghapus data siswa {{ $item->name }}?&quot;)"><i
+                                                                class="fa fa-trash" aria-hidden="true"></i></button>
+                                                    </form>
+                                                @elseif (is_null($item->datapokok))
+                                                    <form method="POST" action="{{ url('/agen' . '/' . $item->id) }}"
+                                                        accept-charset="UTF-8" style="display:inline">
+                                                        {{ method_field('DELETE') }}
+                                                        {{ csrf_field() }}
+                                                        <button type="submit" class="btn btn-danger btn-sm"
+                                                            title="Delete Siswa"
+                                                            onclick="return confirm(&quot;Apakah anda ingin menghapus data siswa {{ $item->name }}?&quot;)"><i
+                                                                class="fa fa-trash" aria-hidden="true"></i></button>
+                                                    </form>
+                                                @else
+                                                    <a href="{{ url('/agen/nilai/' . $item->id) }}"
+                                                        title="Tambah Kelulusan Siswa"><button
+                                                            class="btn btn-success btn-sm"><i class="fa fa-plus"
+                                                                aria-hidden="true"></i></button></a>
+                                                    <a href="{{ url('/agen/cetak/' . $item->id) }}"
+                                                        title="Cetak Datapokok Siswa">
+                                                        <button class="btn btn-warning btn-sm"><i class="fa fa-print"
+                                                                aria-hidden="true"></i></button>
+                                                    </a>
+                                                    <a href="{{ url('/agen/' . $item->id) }}" title="Lihat Siswa"><button
+                                                            class="btn btn-info btn-sm"><i class="fa fa-eye"
+                                                                aria-hidden="true"></i></button></a>
+                                                    {{-- <a href="{{ url('/agen/' . $item->id . '/edit') }}" title="Edit Agen"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a> --}}
+                                                    <form method="POST" action="{{ url('/agen' . '/' . $item->id) }}"
+                                                        accept-charset="UTF-8" style="display:inline">
+                                                        {{ method_field('DELETE') }}
+                                                        {{ csrf_field() }}
+                                                        <button type="submit" class="btn btn-danger btn-sm"
+                                                            title="Delete Siswa"
+                                                            onclick="return confirm(&quot;Apakah anda ingin menghapus data siswa {{ $item->name }}?&quot;)"><i
+                                                                class="fa fa-trash" aria-hidden="true"></i></button>
+                                                    </form>
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            <div class="row mb-5">
+                                <div class="col-md-8">
+                                    Showing {{ $agen->firstItem() }} to {{ $agen->lastItem() }} of {{ $agen->total() }}
+                                </div>
+                                <div class="col-md-4">
+                                    {{-- {{ $siswa->links() }} --}}
+                                    {{ $agen->links() }}
+                                </div>
+                            </div>
+                            <div class="row">
+                                <a href="/excel/sudah-bayar" class="btn btn-success btn-block">Daftar siswa Sudah bayar (Download XLSX)</a>
+                                <a href="/excel/sudah-lulus" class="btn btn-success btn-block">Daftar siswa Sudah lulus (Download XLSX)</a>
+                                <a href="/excel/tidak-lulus" class="btn btn-success btn-block">Daftar siswa Tidak lulus (Download XLSX)</a>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-md-4">
-                        {{-- {{ $siswa->links() }} --}}
-                        {{ $agen->links() }}
-                    </div>
-                </div>
-                <div class="row">
-                    <a href="/excel/sudah-bayar" class="btn btn-success btn-block">Daftar siswa Sudah bayar (Download
-                        XLSX)</a>
-                    <a href="/excel/sudah-lulus" class="btn btn-success btn-block">Daftar siswa Sudah lulus (Download
-                        XLSX)</a>
-                    <a href="/excel/tidak-lulus" class="btn btn-success btn-block">Daftar siswa Tidak lulus (Download
-                        XLSX)</a>
                 </div>
             </div>
         </div>
     </div>
+    <script></script>
 @endsection
