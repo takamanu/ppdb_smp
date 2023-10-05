41b78afccf4b2fa268931a5a9f75e00e89473eec
Takamanu <fauzanalivijsma@gmail.com>
2023-10-05 15:56:04 +0700

    add feature

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -38,7 +38,7 @@
 
     <!-- Page Wrapper -->
     <div id="wrapper">
-            <div class="container">
+            <div class="container mt-5">
                 <div class="row justify-content-center">
                     <div class="col-md-8">
                         <div class="card">
