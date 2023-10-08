1dbd70456910a98508f45a7dba0af8ebabf18df6
Rafael Vito Adrian <87381079+rafaelvitoadrian@users.noreply.github.com>
2023-10-08 15:24:18 +0700

    Update Registration.php

--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Datapokok extends Model
+class Registration extends Model
 {
     use HasFactory;
 
