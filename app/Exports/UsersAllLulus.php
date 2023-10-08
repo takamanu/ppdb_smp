0bdfe84c68d5aae5f165fd1ed3416da2575010f8
rafaelvitoadrian <rafaelvito11@gmail.com>
2023-10-08 13:38:41 +0700

    FIX:EXPORT

--- app/Exports/UsersAllLulus.php
+++ app/Exports/UsersAllLulus.php
@@ -15,12 +15,12 @@ class UsersAllLulus implements FromView
         $users = DB::table('users')
                 ->join('payments', 'users.id', '=', 'payments.user_id')
                 ->join('registration', 'users.id', '=', 'registration.user_id')
-                ->join('testresult', 'registration.id', '=', 'testresult.datapokok_id')
-                ->select('users.*', 'payments.*','registration.nisn','testresult.status')
+                ->join('testResult', 'registration.id', '=', 'testResult.datapokok_id   ')
+                ->select('users.*', 'payments.*','registration.nisn','testResult.status')
                 ->where('users.role',1)
                 ->where('payments.status_payment',2)
                 ->where('payments.status',2)
-                ->where('testresult.status','Lulus')
+                ->where('testResult.status','Lulus')
                 ->get();
         $lulus = "Lulus";
         return view('agen.allsiswabayar', [
--- app/Exports/UsersAllTidakLulus.php
+++ app/Exports/UsersAllTidakLulus.php
@@ -15,12 +15,12 @@ class UsersAllTidakLulus implements FromView
         $users = DB::table('users')
             ->join('payments', 'users.id', '=', 'payments.user_id')
             ->join('registration', 'users.id', '=', 'registration.user_id')
-            ->join('testresult', 'registration.id', '=', 'testresult.datapokok_id')
-            ->select('users.*', 'payments.*','registration.nisn','testresult.status')
+            ->join('testResult', 'registration.id', '=', 'testResult.datapokok_id')
+            ->select('users.*', 'payments.*','registration.nisn','testResult.status')
             ->where('users.role',1)
             ->where('payments.status_payment',2)
             ->where('payments.status',2)
-            ->where('testresult.status','Tidak Lulus')
+            ->where('testResult.status','Tidak Lulus')
             ->get();
         $lulus = "Tidak Lulus";
         return view('agen.allsiswabayar', [
