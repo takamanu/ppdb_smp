661b0410fc34bff93ff95e54d327ed0228a96808
Takamanu <fauzanalivijsma@gmail.com>
2025-04-19 01:09:12 +0700

    logout

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -169,13 +169,13 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
-                <img class="img-profile rounded-circle" src="{{ asset(Auth::user()->avatar) }}">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Kontol</span>
+                {{-- <img class="img-profile rounded-circle" src="{{ asset(Auth::user()->avatar) }}"> --}}
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
 
-                @if (Auth::user()->role == 0)
+                {{-- @if (Auth::user()->role == 0) --}}
                     <a class="dropdown-item" href="/profile">
                         <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                         Profile
@@ -184,12 +184,12 @@
                         <i class="fas fa-key fa-sm fa-fw mr-2 text-gray-400"></i>
                         Config
                     </a>
-                @else
+                {{-- @else
                     <a class="dropdown-item" href="/profile">
                         <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                         Profile
                     </a>
-                @endif
+                @endif --}}
                 {{-- <a class="dropdown-item" href="#">
                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                     Settings
